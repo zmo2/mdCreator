e6b90d64501e65642b39e763f4fa95ad694b753a
ink <eric.zmo2@gmail.com>
2020-03-07 20:47:12 -0800

    check

---
+++ index.js
@@ -0,0 +1,112 @@
+const inquirer = require ("inquirer")
+const fs = require("fs")
+const util = require("util")
+const axios = require("axios")
+
+const writeFileAsync = util.promisify(fs.writeFile)
+
+function promptUser(){
+    return inquirer.prompt([
+        {        
+            type: "input",
+            name: "username",
+            message: "What is your Github username?"
+        },
+        {        
+            type: "input",
+            name: "projName",
+            message: "What is your project username?"
+        },
+        {        
+            type: "input",
+            name: "projDesc",
+            message: "Please write a short description of your project."
+        },
+        {        
+            type: "input",
+            name: "projLicense",
+            message: "What kind of license should your project have?"
+        },
+        {        
+            type: "input",
+            name: "cmdInstall",
+            message: "What command should be run to install dependencies?"
+        },
+        {        
+            type: "input",
+            name: "cmdTest",
+            message: "What command should be run to test?"
+        },
+        {        
+            type: "input",
+            name: "userNTK",
+            message: "What does the user need to know about when using the repo?"
+        },
+        {        
+            type: "input",
+            name: "userContribute",
+            message: "What does the user need to know when contributing to the repo?"
+        }
+    ])
+}
+
+function makeMD(answer,data){
+    return `
+    # ${answer.projName}
+    [![Github license] (https://img.shields.io/badge/license-${answer.projLicense}-brightgreen.svg)] 
+    (https://github.com/zmo2/mdCreator)
+        
+    ## Description
+    ${answer.projDesc}
+    
+    ## Table of Contents
+    
+    *[Installation] (#Installation)
+    *[Usage] (#Usage)
+    *[License] (#License)
+    *[Contributing] (#Contributing)
+    *[Tests] (#Tests)
+    *[Questions] (#Questions)
+
+    ## Installation:
+    To install neccessary dependencies, run the following command:
+
+       ${answer.cmdInstall}
+    
+    ## Usage
+
+       ${answer.userNTK}
+
+    ## License
+    This project is licensed under the ${answer.projLicense} license.
+
+    ## Contributing
+    ${answer.userContribute}
+
+    ## Tests
+    To run tests, run the following command:
+    ${answer.cmdTest}
+
+    ## Questions
+    <img src="${data.avatar_url}" alt="avatar" style="border-radius:16px" width="30"/>
+
+    If you hae questions about the repo, please contact [${answer.username}] (https://github.com/${answer.username}) directly. 
+
+    `
+}
+
+async function init(){
+    try{
+        const answer = await promptUser()
+        const {data} = await axios.get(`https://api.github.com/users/${answer.username}`)
+        console.log(data.avatar_url)
+        const mkMD = makeMD(answer,data)
+        await writeFileAsync("readme.md", mkMD)
+        console.log("successfully wrote to readme.md")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+init()
---
+++ readme.md
@@ -0,0 +1,42 @@
+
+    # Readme Creator
+    [![Github license] (https://img.shields.io/badge/license-mit-brightgreen.svg)] 
+    (https://github.com/zmo2/mdCreator)
+        
+    ## Description
+    makes a readme
+    
+    ## Table of Contents
+    
+    *[Installation] (#Installation)
+    *[Usage] (#Usage)
+    *[License] (#License)
+    *[Contributing] (#Contributing)
+    *[Tests] (#Tests)
+    *[Questions] (#Questions)
+
+    ## Installation:
+    To install neccessary dependencies, run the following command:
+
+       npm i
+    
+    ## Usage
+
+       nothin
+
+    ## License
+    This project is licensed under the mit license.
+
+    ## Contributing
+    contri
+
+    ## Tests
+    To run tests, run the following command:
+    npm test
+
+    ## Questions
+    <img src="https://avatars2.githubusercontent.com/u/34842627?v=4" alt="avatar" style="border-radius:16px" width="30"/>
+
+    If you hae questions about the repo, please contact [zmo2] (https://github.com/zmo2) directly. 
+
+    
